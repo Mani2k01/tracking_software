41aa4699443d8a4452448e8ec2b8947dea6f40e8
Mani2K01 <cmani498@gmail.com>
2026-03-23 17:04:31 +0530

    cloud db added

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Order_tracker",
-  password: "Manikandan12",
-  port: "5432",
+//   user: "postgres",
+//   host: "localhost",
+//   database: "Order_tracker",
+//   password: "Manikandan12",
+//   port: "5432",
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
